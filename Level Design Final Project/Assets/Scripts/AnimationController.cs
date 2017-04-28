80ebb3d8dea796ec75d3b2e858c18f95378afbc0
Whittaker <whitt117@mail.chapman.edu>
2017-04-27 18:42:32 -0700

    Improves character animation.

---
+++ Level Design Final Project/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        myAnimator = GetComponent<Animtor>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        myAnimator.SetFloat ("VSpeed, Input.GetAxis ("Vertical");
+		
+	}
+}
